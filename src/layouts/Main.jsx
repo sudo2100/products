24a5c37fcdfae6e81687b930759e9834c840a129
sudo2100 <sudo2100@naver.com>
2026-06-20 06:31:02 +0900

    products css

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -6,6 +6,7 @@ const Main = () => {
   return(
     <div className="main">
       <h1>컴퓨터 기기 쇼핑몰</h1>
+      <h3>최신 컴퓨터 기기를 한 곳에서 만나보세요!</h3>
       <img src={mainImage} alt="메인이미지" />
     </div>
   )
